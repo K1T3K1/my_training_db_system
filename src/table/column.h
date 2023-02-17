18c84abdebc0796006eec569380accdf41771313
Kajetan Patryk Zarzycki <kp.zarzycki@gmail.com>
2023-02-17 20:05:44 +0100

    Further column changes split column into two classes column and column_impl column serves as a polymorphic wrapper for columns placed inside a table

--- src/table/column.h
+++ src/table/column.h
@@ -4,21 +4,27 @@
 #include <functional>
 #include <stdexcept>
 #include <type_traits>
+#include <memory>
+#include <variant>
 
 // Still not decided whether should I choose a template column
 // or polymorphic columns with distinctions for each individual
 // data types
 
+typedef __int128_t integral;
+typedef long double numeric;
+typedef char varchar[255];
+
 template <typename data_t>
-struct column {
-  column<data_t>(std::function<bool(data_t)> _constraint) : constraint(_constraint){}
-  column<data_t>(std::function<bool(data_t)> _constraint, bool _is_nullable)
+struct column_impl {
+  column_impl<data_t>(std::function<bool(data_t)> _constraint) : constraint(_constraint) {}
+  column_impl<data_t>(std::function<bool(data_t)> _constraint, bool _is_nullable)
       : constraint(_constraint), is_nullable(_is_nullable) {}
 
   // This constructor requires to validate whether provided data
   // satisfies provided constraint. If constraint isn't satisfied
   // an exception gets thrown. It needs to be handled
-  column<data_t>(std::function<bool(data_t)> _constraint, bool _is_nullable, data_t _value)
+  column_impl<data_t>(std::function<bool(data_t)> _constraint, bool _is_nullable, data_t _value)
       : constraint(_constraint), is_nullable(_is_nullable) {
     if (validate_data()) {
       value = _value;
@@ -30,6 +36,7 @@ struct column {
   std::function<bool(data_t)> constraint;
   bool is_nullable = false;
   data_t value;
+  auto get_column_type_variant(){return std::variant<data_t>();}
 
   bool validate_data() { return constraint(value); }
 
@@ -39,8 +46,12 @@ struct column {
   }
 };
 
-typedef __int128_t integral;
-typedef long double numeric;
-typedef char varchar[255];
+struct column {
+  typedef column_impl<integral> column_int;
+  typedef column_impl<numeric> column_num;
+  typedef column_impl<varchar> column_varchar;
+
+  std::variant<column_int, column_num, column_varchar> column_object;
+};
 
 #endif
--- src/table/table.h
+++ src/table/table.h
@@ -1,26 +1,24 @@
 #ifndef TABLE_H_
 #define TABLE_H_
 
+#include <span>
+#include <string>
+#include "column.h"
+
 // Both tables and rows will need simple interfaces to be parsed
 // from and to b-trees that will allow to perform fast operations
 // on data
 // Read, Write operations themselves will suffice without these
 // data projections
 
-struct table_interface{
+struct row{
 
 };
 
-struct table : table_interface{
-
-};
-
-struct row_interface{
-
-};
-
-struct row : row_interface{
-
+struct table{
+    std::string name;
+    std::span<std::unique_ptr<column>> columns;
+    std::span<std::unique_ptr<row>> rows;
 };
 
 #endif
